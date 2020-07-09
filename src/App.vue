4679d5f3ba4604ea9479653361b8e9ff8cae285a
Akunin94 <35231300+Akunin94@users.noreply.github.com>
2020-07-09 18:48:21 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header />
+    <Carousel />
+    <CarouselModal />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Carousel from './components/Carousel.vue'
+import CarouselModal from './components/CarouselModal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Carousel,
+    CarouselModal
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,67 @@
+<template>
+   <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel" data-interval="4000">
+        <ol class="carousel-indicators">
+            <li v-for="(slide, index) in slides" :key="slide.index" data-target="#carouselExampleControls" :data-slide-to="index" :class="{ active: index == 0 }"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div v-for="(slide, index) in slides" :key="slide.index" class="carousel-item" :class="{ active: index == 0 }" :data-interval="slide.interval">
+                <img class="d-block w-100" :src="slide.icon" :alt="slide.alt" :title="slide.title">
+                <div v-if="slide.caption" class="carousel-caption d-none d-lg-block">
+                    <h5 v-if="slide.caption.title">First slide label</h5>
+                    <p v-if="slide.caption.body">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <button v-if="slide.caption.button" type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">Launch demo modal</button>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div> 
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          slides: [
+                { 
+                    title: 'Pic1', 
+                    alt: 'Pic1', 
+                    icon: require('@/assets/t1.jpg'),
+                    caption: {
+                        title: "Заголовок",
+                        body : "Тестовый текст",
+                        button: true
+                    }
+                },
+                { 
+                    title: 'Pic2', 
+                    alt: 'Pic2', 
+                    icon: require('@/assets/t2.jpg'),
+                    caption: {
+                        title: "Заголовок1"
+                    },
+                    interval: 1000
+                },
+                { 
+                    title: 'Pic3', 
+                    alt: 'Pic3', 
+                    icon: require('@/assets/t3.jpg') 
+                }
+          ]
+      }
+  }
+}
+</script>
+
+<style scoped>
+.carousel-control-next-icon, .carousel-control-prev-icon {
+    width: 50px;
+    height: 50px;
+}
+</style>
