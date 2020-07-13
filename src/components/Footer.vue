d47bafb132a5dc150cd8a1d91930bb4fc722e691
Akunin94 <35231300+Akunin94@users.noreply.github.com>
2020-07-13 18:32:07 +0500

    Footer added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,18 @@
 <template>
-    <footer class="container-fluid py-4 text-muted" style="background: rgba(0, 0, 0, .05)">
-        <ul class="bd-footer-links">
-            <li><a href="https://github.com/twbs">GitHub</a></li>
-            <li><a href="https://twitter.com/getbootstrap">Twitter</a></li>
-            <li><a href="/docs/4.5/examples/">Examples</a></li>
-            <li><a href="/docs/4.5/about/overview/">About</a></li>
+    <footer class="container-fluid py-4 text-muted text-center" style="background: rgba(0, 0, 0, .05)">
+        <ul class="nav nav-pills mb-3 justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="https://github.com/twbs">GitHub</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com/getbootstrap">Twitter</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/docs/4.5/examples/">Examples</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/docs/4.5/about/overview/">About</a>
+            </li>
         </ul>
         <p>Designed and built with all the love in the world by the <a href="/docs/4.5/about/team/">Bootstrap team</a> with the help of <a href="https://github.com/twbs/bootstrap/graphs/contributors">our contributors</a>.</p>
         <p>Currently v4.5.0. Code licensed <a href="https://github.com/twbs/bootstrap/blob/main/LICENSE" target="_blank" rel="license noopener">MIT</a>, docs <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="license noopener">CC BY 3.0</a>.</p>
